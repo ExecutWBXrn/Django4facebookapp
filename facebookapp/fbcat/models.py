c82cddc31f78d9e406e87499b922e01c989f0b13
ExecutWBXrn <executwbxrn@gmail.com>
2024-01-13 18:23:52 +0000

    add foreingkey

--- facebookapp/fbcat/models.py
+++ facebookapp/fbcat/models.py
@@ -9,6 +9,7 @@ class facebook(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT)
 
     def __str__(self):
         return self.title
@@ -18,3 +19,10 @@ class facebook(models.Model):
 
     def get_absolute_url(self):
         return reverse("profile", kwargs={"pro_slug":self.slug})
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=99, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
