7f153c64fb3f64ac318733168d8eb208be2caf35
ExecutWBXrn <executwbxrn@gmail.com>
2024-01-25 21:13:02 +0000

    upgrade admin panel

--- facebookapp/fbcat/admin.py
+++ facebookapp/fbcat/admin.py
@@ -1,13 +1,27 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import *
 
 @admin.register(facebook)
 class AdminFacebook(admin.ModelAdmin):
-    list_display = ['id', 'title', 'time_create', 'time_update', 'is_published', 'cat']
+    list_display = ['id', 'title', 'time_create', 'time_update', 'is_published', 'cat', "brief_info"]
     list_display_links = ['id', 'title']
     list_editable = ['is_published']
     list_per_page = 5
     ordering = ['time_update', 'title']
+    actions = ["set_published", "set_draft"]
+
+    @admin.display(description="Коротка інформація", ordering="describe")
+    def brief_info(self, facebook: facebook):
+        return f"кількість символів в описі: {len(facebook.describe)}"
+    @admin.action(description="set published")
+    def set_published(self, request, queryset):
+        q = queryset.update(is_published=facebook.STATUS.PUBLISHED)
+        self.message_user(request, f"опубліковано {q} фото")
+
+    @admin.action(description="set draft")
+    def set_draft(self, request, queryset):
+        q = queryset.update(is_published=facebook.STATUS.DRAFT)
+        self.message_user(request, f"деопубліковано {q} фото", messages.WARNING)
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
